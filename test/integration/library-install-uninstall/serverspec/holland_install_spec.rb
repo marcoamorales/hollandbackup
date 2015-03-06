9a6012e8b31ceaee16b59b0ac09cd71039ac44c7
Brint O'Hearn <brint.ohearn@rackspace.com>
2015-03-06 16:14:25 -0600

    Adding serverspec tests for install and uninstall

---
+++ test/integration/library-install-uninstall/serverspec/holland_install_spec.rb
@@ -0,0 +1,14 @@
+require 'serverspec'
+
+set :backend, :exec
+
+describe package('holland') do
+  it { is_expected.to_not be_installed }
+end
+
+describe file('/etc/holland') do
+  it { should be_directory }
+  it { should be_owned_by 'root' }
+  it { should be_grouped_into 'root' }
+  it { should be_mode 755 }
+end
