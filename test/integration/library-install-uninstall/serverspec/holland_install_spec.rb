4d5b7705df43a6e48e3ec44571782096543e01cc
Brint O'Hearn <brint.ohearn@rackspace.com>
2015-03-06 17:17:00 -0600

    Updating directory tests for epel installs

--- test/integration/library-install-uninstall/serverspec/holland_install_spec.rb
+++ test/integration/library-install-uninstall/serverspec/holland_install_spec.rb
@@ -6,9 +6,11 @@ describe package('holland') do
   it { is_expected.to_not be_installed }
 end
 
-describe file('/etc/holland') do
-  it { should be_directory }
-  it { should be_owned_by 'root' }
-  it { should be_grouped_into 'root' }
-  it { should be_mode 755 }
+if ['debian', 'ubuntu'].include?(os[:family])
+  describe file('/etc/holland') do
+    it { should be_directory }
+    it { should be_owned_by 'root' }
+    it { should be_grouped_into 'root' }
+    it { should be_mode 755 }
+  end
 end
